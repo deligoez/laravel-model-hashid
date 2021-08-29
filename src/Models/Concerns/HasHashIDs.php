6cd845cb1205253911bc3804ed1e5ffce244a6e1
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-29 10:06:09 +0300

    Model hashID salt can be defined

--- src/Models/Concerns/HasHashIDs.php
+++ src/Models/Concerns/HasHashIDs.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 namespace Deligoez\LaravelModelHashIDs\Models\Concerns;
 
+use Hashids\Hashids;
+use Hashids\HashidsInterface;
+use Config;
+
 trait HasHashIDs
 {
+    protected ?HashidsInterface $hashIDGenerator = null;
 
+    /**
+     * Initialize the HasHasIDs trait for an instance.
+     *
+     * @return void
+     */
+    public function initializeHasHashIDs(): void
+    {
+        $salt = Config::get('hashids.salt', '');
+
+        $this->hashIDGenerator = new Hashids($salt);
+    }
 }
--- tests/HasHasIDTest.php
+++ tests/HasHasIDTest.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace Deligoez\LaravelModelHashIDs\Tests;
 
+use Deligoez\LaravelModelHashIDs\Tests\Models\ModelA;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Config;
 class HasHasIDTest extends TestCase
 {
+    use RefreshDatabase;
+    /** @test */
+    public function model_hashID_salt_can_be_defined(): void
+    {
+        // 1️⃣ Arrange 🏗
+        $model = ModelA::factory()->create();
+        $hash = $model->hashID;
+
+        // 2️⃣ Act 🏋🏻‍
+        Config::set('hashids.salt', Str::random());
+
+        // 3️⃣ Assert ✅
+        $newHash = ModelA::findOrFail($model->getKey())->hashID;
+        $this->assertNotEquals($hash, $newHash);
+    }
 }
