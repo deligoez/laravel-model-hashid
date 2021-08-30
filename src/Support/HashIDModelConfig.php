7177f121ea3a90be050e4fe2051fecd7cf330404
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-30 09:18:51 +0300

    HashIDModelConfig@forModel: Get parameters from config

--- src/Support/HashIDModelConfig.php
+++ src/Support/HashIDModelConfig.php
@@ -5,30 +5,25 @@ declare(strict_types=1);
 namespace Deligoez\LaravelModelHashIDs\Support;
 
 use Config;
-use RuntimeException;
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use RuntimeException;
 
 class HashIDModelConfig
 {
     public static function forModel(Model $model, string $config): string|int
     {
-        if (! in_array($config, [
-            'salt',
-            'length',
-            'alphabet',
-            'prefix_length',
-            'prefix_case',
-            'separator',
-            'generators',
-        ], true)) {
+        // Check if given $config is defined on config file
+        if (!in_array($config, array_keys(Config::get('hashids')), true)) {
             throw new RuntimeException("Unknown config parameter: '{$config}'.");
         }
 
+        // Return specific config for model if defined
         if (Arr::has(Config::get('hashids.generators'), get_class($model).'.'.$config)) {
             return Config::get('hashids.generators')[get_class($model)][$config];
         }
 
+        // Return generic config
         return Config::get("hashids.{$config}");
     }
 }
