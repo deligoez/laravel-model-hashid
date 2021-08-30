e43d9007e5a556b4f0635afa40ae989e13ac2861
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-30 19:10:04 +0300

    Create `ModelHashID` class

---
+++ src/Support/ModelHashID.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deligoez\LaravelModelHashIDs\Support;
+
+class ModelHashID
+{
+    public function __construct(
+        public string $prefix,
+        public string $separator,
+        public string $hashIDForKey,
+        public string $modelClassName
+    ) {
+    }
+}
