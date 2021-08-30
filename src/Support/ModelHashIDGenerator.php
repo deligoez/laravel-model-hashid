146abb1b08ff65740afbabba36aa63a57ece8c5c
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-30 10:28:47 +0300

    ModelHashIDGenerator@buildPrefixForModel: Get config via `HashIDModelConfig`

--- src/Support/ModelHashIDGenerator.php
+++ src/Support/ModelHashIDGenerator.php
@@ -14,10 +14,10 @@ class ModelHashIDGenerator
     public static function buildPrefixForModel(Model $model): string
     {
         $shortClassName = (new ReflectionClass($model))->getShortName();
-        $prefixLength = Config::get('hashids.prefix_length', 3); // for model or generic
+        $prefixLength = HashIDModelConfig::forModel($model, config: 'prefix_length');
         $prefix = rtrim(mb_strimwidth($shortClassName, 0, $prefixLength, '', 'UTF-8'));
 
-        return match (Config::get('hashids.prefix_case', 'lower')) {
+        return match (HashIDModelConfig::forModel($model, 'prefix_case')) {
             'upper'         => Str::upper($prefix),
             'camel'         => Str::camel($prefix),
             'snake'         => Str::snake($prefix),
