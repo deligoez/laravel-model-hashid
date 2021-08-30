58e895e8b6c58fb5e163bc90282796f69728b0f9
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-30 11:22:05 +0300

    HashIDModelConfig: Refactor validations into methods

--- src/Support/HashIDModelConfig.php
+++ src/Support/HashIDModelConfig.php
@@ -13,20 +13,10 @@ class HashIDModelConfig
 {
     public static function get(Model|string $model, string $config): string|int
     {
-        // Check if given $config is defined on config file
-        if (!in_array($config, array_keys(Config::get('hashids')), true)) {
-            throw new RuntimeException("Unknown config parameter: '{$config}'.");
-        }
+        self::isConfigParameterDefined($config);
+        self::modelClassExists($model);
 
-        if ($model instanceof Model) {
-            $className = get_class($model);
-        } else {
-            // Check if given FQCN exists
-            if (!class_exists($model)) {
-                throw new RuntimeException("Model not exists: '{$config}'.");
-            }
-            $className = $model;
-        }
+        $className = $model instanceof Model ? get_class($model) : $model;
 
         // Return specific config for model if defined
         if (Arr::has(Config::get('hashids.generators'), $className.'.'.$config)) {
@@ -35,5 +25,19 @@ class HashIDModelConfig
 
         // Return generic config
         return Config::get("hashids.{$config}");
+    }
+
+    private static function isConfigParameterDefined(string $config): void
+    {
+        if (!in_array($config, array_keys(Config::get('hashids')), true)) {
+            throw new RuntimeException("Unknown config parameter: '{$config}'.");
+        }
+    }
+
+    private static function modelClassExists(Model|string $model): void
+    {
+        if (is_string($model) && !class_exists($model)) {
+            throw new RuntimeException("Model not exists: '{$model}'.");
+        }
     }
 }
