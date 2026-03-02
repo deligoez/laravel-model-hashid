c9bb76ba70500380893abe0046ebea06370ff8fa
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-03 00:52:31 +0300

    chore(Pest): remove unused WithFaker trait (Pest has built-in fake helper)

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
-use Illuminate\Foundation\Testing\WithFaker;
 use Deligoez\LaravelModelHashId\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(TestCase::class, RefreshDatabase::class, WithFaker::class)->in(__DIR__);
+uses(TestCase::class, RefreshDatabase::class)->in(__DIR__);
