8661c1019683d49f60b9a3de5915a4c6e874df65
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-03 02:04:47 +0300

    test(ArchitectureTest): add arch tests for Casts and Commands namespaces (architecture coverage)

--- tests/Architecture/ArchitectureTest.php
+++ tests/Architecture/ArchitectureTest.php
@@ -17,3 +17,11 @@ arch('traits are traits')
 arch('mixins are classes')
     ->expect('Deligoez\LaravelModelHashId\Mixins')
     ->toBeClasses();
+
+arch('casts are classes')
+    ->expect('Deligoez\LaravelModelHashId\Casts')
+    ->toBeClasses();
+
+arch('commands are classes')
+    ->expect('Deligoez\LaravelModelHashId\Commands')
+    ->toBeClasses();
