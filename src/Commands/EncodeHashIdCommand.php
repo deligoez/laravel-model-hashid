4aa72eb944c41d9e4d0c68df150cbfec62df79c4
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-03 02:01:50 +0300

    feat(EncodeHashIdCommand): add hashid:encode artisan command (CLI hash id generation)

---
+++ src/Commands/EncodeHashIdCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deligoez\LaravelModelHashId\Commands;
+
+use Illuminate\Console\Command;
+use Deligoez\LaravelModelHashId\Traits\HasHashId;
+
+class EncodeHashIdCommand extends Command
+{
+    protected $signature = 'hashid:encode {model : The fully qualified model class name} {id : The integer key to encode}';
+
+    protected $description = 'Encode an integer key to a hash ID for a given model';
+
+    public function handle(): int
+    {
+        /** @var string $modelClass */
+        $modelClass = $this->argument('model');
+        $id         = (int) $this->argument('id');
+
+        if (!class_exists($modelClass)) {
+            $this->error("Class [{$modelClass}] does not exist.");
+
+            return self::FAILURE;
+        }
+
+        if (!in_array(HasHashId::class, class_uses_recursive($modelClass), true)) {
+            $this->error("Class [{$modelClass}] does not use the HasHashId trait.");
+
+            return self::FAILURE;
+        }
+
+        /** @var \Illuminate\Database\Eloquent\Model&HasHashId $model */
+        $model = new $modelClass();
+        $model->{$model->getKeyName()} = $id;
+
+        $this->info($model->hashId);
+
+        return self::SUCCESS;
+    }
+}
