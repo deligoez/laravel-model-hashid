1208af245a11a7f2e5adfe9936ac99f974db4ba6
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-03 00:29:30 +0300

    feat(ArchitectureTest): add architecture tests (enforce structural rules)

---
+++ tests/Architecture/ArchitectureTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+arch('no debugging functions are used')
+    ->expect(['dd', 'dump', 'ray', 'var_dump', 'print_r'])
+    ->not->toBeUsed();
+
+arch('all source files use strict types')
+    ->expect('Deligoez\LaravelModelHashId')
+    ->toUseStrictTypes();
+
+arch('traits are traits')
+    ->expect('Deligoez\LaravelModelHashId\Traits')
+    ->toBeTraits();
+
+arch('mixins are classes')
+    ->expect('Deligoez\LaravelModelHashId\Mixins')
+    ->toBeClasses();
